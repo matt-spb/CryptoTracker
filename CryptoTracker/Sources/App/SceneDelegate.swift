bbeaa50d6a481edd0bc76f65316f7ccf4d6fd937
Matvey Fedyshin <mnf89@mail.ru>
2024-02-10 22:41:42 +0300

    Initial commit

---
+++ CryptoTracker/Sources/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  CryptoTracker
+//
+//  Created by Матвей Федышин on 04.12.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        
+        window?.makeKeyAndVisible()
+    }
+}
+
